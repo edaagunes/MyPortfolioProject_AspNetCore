4d28be60f64ad37213c52f004713e41dae954dcf
edaagunes <edagunes00@hotmail.com>
2024-11-09 22:07:13 +0300

    Add project files.

---
+++ MyPortfolio_Asp.NetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyPortfolio_Asp.NetCore.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
