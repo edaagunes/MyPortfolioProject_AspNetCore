5c989b6187f5acc488e18ec51d635139de6f82a7
edaagunes <edagunes00@hotmail.com>
2024-11-10 01:08:25 +0300

    Added Feature and About

--- MyPortfolio_Asp.NetCore/ViewComponents/_FeatureComponentPartial.cs
+++ MyPortfolio_Asp.NetCore/ViewComponents/_FeatureComponentPartial.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio_Asp.NetCore.DAL.Context;
 
 namespace MyPortfolio_Asp.NetCore.ViewComponents
 {
-    public class _FeatureComponentPartial:ViewComponent
+    public class _FeatureComponentPartial : ViewComponent
     {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values=portfolioContext.Features.ToList();
+            return View(values);
         }
     }
 }
