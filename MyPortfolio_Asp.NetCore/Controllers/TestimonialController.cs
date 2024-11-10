8f8135da22e0704ddbac0b9130fb973f2f9d1b4c
edaagunes <edagunes00@hotmail.com>
2024-11-10 21:05:53 +0300

    Completed Testimonial Controller

---
+++ MyPortfolio_Asp.NetCore/Controllers/TestimonialController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio_Asp.NetCore.DAL.Context;
+using MyPortfolio_Asp.NetCore.DAL.Entities;
+
+namespace MyPortfolio_Asp.NetCore.Controllers
+{
+	public class TestimonialController : Controller
+	{
+		MyPortfolioContext context = new MyPortfolioContext();
+		public IActionResult Index()
+		{
+			var values = context.Testimonials.ToList();
+			return View(values);
+		}
+		[HttpGet]
+		public IActionResult CreateTestimonial()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult CreateTestimonial(Testimonial testimonial)
+		{
+			context.Testimonials.Add(testimonial);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult DeleteTestimonial(int id)
+		{
+			var value = context.Testimonials.Find(id);
+			context.Testimonials.Remove(value);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public IActionResult UpdateTestimonial(int id)
+		{
+			var values = context.Testimonials.Find(id);
+			return View(values);
+		}
+		[HttpPost]
+		public IActionResult UpdateTestimonial(Testimonial testimonial)
+		{
+			context.Testimonials.Update(testimonial);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
