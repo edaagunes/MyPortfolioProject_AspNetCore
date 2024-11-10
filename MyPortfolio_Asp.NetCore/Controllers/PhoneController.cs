948b10e897a5c3909d4027cab5178fcebba861fa
edaagunes <edagunes00@hotmail.com>
2024-11-10 20:25:48 +0300

    Completed Phone Controller

---
+++ MyPortfolio_Asp.NetCore/Controllers/PhoneController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio_Asp.NetCore.DAL.Context;
+using MyPortfolio_Asp.NetCore.DAL.Entities;
+
+namespace MyPortfolio_Asp.NetCore.Controllers
+{
+	public class PhoneController : Controller
+	{
+		MyPortfolioContext context=new MyPortfolioContext();
+		public IActionResult Index()
+		{
+			var values=context.Contacts.ToList();
+			return View(values);
+		}
+		[HttpGet]
+		public IActionResult UpdatePhone(int id)
+		{
+			var values = context.Contacts.Find(id);
+			return View(values);
+		}
+		[HttpPost]
+		public IActionResult UpdatePhone(Contact contact)
+		{
+			context.Contacts.Update(contact);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
