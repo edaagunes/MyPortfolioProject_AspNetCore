ff1345e028ca6f0f86d1cb73902d863da19b5086
edaagunes <edagunes00@hotmail.com>
2024-11-10 14:13:58 +0300

    Added Admin Panel

---
+++ MyPortfolio_Asp.NetCore/Controllers/ExperienceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio_Asp.NetCore.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        public IActionResult ExperienceList()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyPortfolio_Asp.NetCore/Controllers/LayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio_Asp.NetCore.Controllers
+{
+    public class LayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
