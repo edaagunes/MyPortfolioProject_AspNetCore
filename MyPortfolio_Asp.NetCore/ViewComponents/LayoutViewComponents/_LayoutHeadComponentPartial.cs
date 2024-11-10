fa61856c58ee26d79c01c46dc889d3f6708379e8
edaagunes <edagunes00@hotmail.com>
2024-11-10 14:42:02 +0300

    Added View Components in Admin

---
+++ MyPortfolio_Asp.NetCore/ViewComponents/LayoutViewComponents/_LayoutHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio_Asp.NetCore.ViewComponents.LayoutViewComponents
+{
+	public class _LayoutHeadComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
